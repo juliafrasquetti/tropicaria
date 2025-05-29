424bebae938f080c101f26d2ffc6be7773e04284
juliafrasquetti <juliafrasquetti10@gmail.com>
2025-05-29 10:48:48 -0300

    TR

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const botaoDeAcessibilidade = document.getElementById ('botao-acessibilidade')
+    const opcoesDeAcessilidade = document.getElementById('OPCOES-ACESSIBILIDADE')
+
+    
+}
